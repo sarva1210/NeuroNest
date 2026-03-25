5358f7d0b8d97aaea968a85e8fcd72d13cfd671c
sarva1210 <sarva.vish77@gmail.com>
2026-03-25 21:46:48 +0530

    json packages

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,22 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./modules/auth/auth.routes.js";
+import userRoutes from "./modules/user/user.routes.js";
+import itemRoutes from "./modules/item/item.routes.js";
+import { errorHandler } from "./middleware/error.middleware.js";
+import searchRoutes from "./modules/search/search.routes.js";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/items", itemRoutes);
+app.use("/api/search", searchRoutes);
+
+app.use(errorHandler);
 
 export default app;
