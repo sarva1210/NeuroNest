b57c09952cefd20072338004ef10e80cbd893059
sarva1210 <sarva.vish77@gmail.com>
2026-03-26 21:04:27 +0530

    dlt files

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import { connectDB } from "./config/db.js";
-import { env } from "./config/env.js";
-
-connectDB();
-
-app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}`);
-});
