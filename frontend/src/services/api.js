feba9d1fc21e4b6f6b6338bbba439b8af210a49b
sarva1210 <sarva.vish77@gmail.com>
2026-04-02 19:34:26 +0530

    adding features

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,17 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "http://localhost:3000/api",
-  withCredentials: true
+});
+
+// INTERCEPTOR
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default API;
