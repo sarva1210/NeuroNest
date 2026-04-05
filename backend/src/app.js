192c0575430b20ce94779e46e3e452824d90570e
sarva1210 <sarva.vish77@gmail.com>
2026-04-05 20:41:44 +0530

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,26 +20,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // CORS CONFIG
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://neuronnest-2.onrender.com",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("CORS not allowed"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
